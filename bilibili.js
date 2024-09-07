e23cedbbfe94c01ffd48628cd96d745b43911d59
Windy-Hill <69096869+Windy-Hill@users.noreply.github.com>
2024-09-07 18:42:28 +0800

    bilibili.js

---
+++ bilibili.js
@@ -0,0 +1,6 @@
+const headers = {
+    'Host': 'tf-i0.hdslb.com',
+    'Connection': 'keep-alive',
+    'User-Agent': 'tf:ct',  // 注意：这里的User-Agent你可能需要根据实际情况修改
+    'Accept-Encoding': 'gzip, deflate, br'
+};
